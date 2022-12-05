8b610e232a64172ed7d64bc964d4a7e859c777bb
Abhijeet03s <abhijeetsharma476@gmail.com>
2022-12-05 15:05:03 +0530

    timestamp added

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
-const todoSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-    trim: true,
-    unique: true,
-    maxLength: [15, "Title should not exceeds more than 20 characters"],
+const todoSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+      unique: true,
+      maxLength: [15, "Title should not exceeds more than 20 characters"],
+    },
+    tasks: [],
   },
-  tasks: [],
-});
-
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("TodoSchema", todoSchema);
